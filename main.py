73b4815717a0226ed66b3182ddccfd62fc2fb22b
Maxima <maxime.arthaud@gmail.com>
2013-03-06 21:50:50 +0100

    Fix commit 499b2a making port argument facultatif.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from server import Server
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Launch a server for the whitenight game.')
-    parser.add_argument('host', default='0.0.0.0', nargs='?')
-    parser.add_argument('port', default=4321, type=int, nargs='?')
+    parser.add_argument('--host', default='0.0.0.0')
+    parser.add_argument('-p', '--port', default=4321, type=int)
     parser.add_argument('map_file')
     args = parser.parse_args()
     Server(args.host, args.port, args.map_file).run()
