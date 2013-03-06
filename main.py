f94309a2382c42e91e590f83bd9e651fc9d8b627
palkeo <palkeo@gmail.com>
2013-03-06 21:37:35 +0100

    Default port to 4321

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from server import Server
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Launch a server for the whitenight game.')
     parser.add_argument('host', default='0.0.0.0', nargs='?')
-    parser.add_argument('port', type=int)
+    parser.add_argument('port', default=4321, type=int)
     parser.add_argument('map_file')
     args = parser.parse_args()
     Server(args.host, args.port, args.map_file).run()
